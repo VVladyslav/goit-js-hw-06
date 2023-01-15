d1c04dfdef80531a32fa099d4111e66c6516bcc2
VVladyslav <VVVasylyshyn@gmail.com>
2023-01-15 13:27:58 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const input = document.querySelector("#font-size-control");
 const output = document.querySelector("#text");
-input.addEventListener("change", textSizeHandler);
+input.addEventListener("input", textSizeHandler);
 
 function textSizeHandler(event) {
   const range = event.currentTarget.value;
